29b1ba3528bc94085d6512e0a1ae159e1ca606b7
Tanya Selvog <tanyaselvog@gmail.com>
2021-03-20 13:56:17 -0500

    removed comments for assignment rendering

--- src/App.jsx
+++ src/App.jsx
@@ -48,9 +48,7 @@ class App extends React.Component {
 
   render() {
     let tabChoice = <div />;
-
-    /*Uncomment below to render assignments*/
-    /*if (this.state.buttonClicked === "assignments") {
+    if (this.state.buttonClicked === "assignments") {
       tabChoice = (
         <List
           placeholder="Add Assignment..."
@@ -59,11 +57,11 @@ class App extends React.Component {
           title="Assignments"
         />
       );
-    }*/
+    }
 
     /* Change below to render students*/
 
-    /*if (this.state.buttonClicked === "students") {
+    if (this.state.buttonClicked === "students") {
       tabChoice = (
         <List
           placeholder="Add Assignment..." 
@@ -72,10 +70,9 @@ class App extends React.Component {
           title="Student Roster"
         />
       );
-    }*/
+    }
 
-    /* Uncomment lines below to render grades*/
-    /*if (this.state.buttonClicked === "grades") {
+    if (this.state.buttonClicked === "grades") {
       tabChoice = (
         <Table
           tableNames={this.state.assignments}
@@ -84,7 +81,7 @@ class App extends React.Component {
           data={this.state.grades}
         />
       );
-    }*/
+    }
 
     return (
       <div>
